49fb388feda19ab74f91f14fcaf58968b4375c84
Vũ Văn Thanh <vuthanhtb.tech@gmail.com>
2025-12-17 00:57:49 +0700

    update

--- common/common-domain/src/main/java/com/thanhvv/food/ordering/system/domain/valueobject/Money.java
+++ common/common-domain/src/main/java/com/thanhvv/food/ordering/system/domain/valueobject/Money.java
@@ -4,35 +4,26 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Objects;
 
-public class Money {
-    private final BigDecimal amount;
-
-    public Money(BigDecimal amount) {
-        this.amount = amount;
-    }
+public record Money(BigDecimal amount) {
 
     public boolean isGreaterThanZero() {
         return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
     }
 
     public boolean isGreaterThan(Money money) {
-        return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
+        return this.amount != null && this.amount.compareTo(money.amount()) > 0;
     }
 
     public Money add(Money money) {
-        return new Money(this.setScale(this.amount.add(money.getAmount())));
+        return new Money(this.setScale(this.amount.add(money.amount())));
     }
 
     public Money subtract(Money money) {
-        return new Money(this.setScale(this.amount.subtract(money.getAmount())));
+        return new Money(this.setScale(this.amount.subtract(money.amount())));
     }
 
     public Money multiply(int multiplier) {
         return new Money(this.setScale(this.amount.multiply(new BigDecimal(multiplier))));
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
     }
 
     @Override
@@ -40,11 +31,6 @@ public class Money {
         if (o == null || getClass() != o.getClass()) return false;
         Money money = (Money) o;
         return Objects.equals(amount, money.amount);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(amount);
     }
 
     private BigDecimal setScale(BigDecimal input) {
